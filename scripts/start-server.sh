e5e3d6221369be203b7c223e8f0e2ade10186e47
ich777 <ich777@noreply.example.org>
2020-01-07 19:36:24 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -4,3 +4,12 @@ umask ${UMASK}
 
 echo "---Sleep zZz---"
 sleep infinity
+
+cd ${DATA_DIR}
+wget -q -nc --show-progress --progress=bar:force:noscroll "${DL_URI}"
+tar -xvf kaiEngine*
+rm -R kaiEngine*.tar.gz
+cd kaiEngine*
+mv * ${DATA_DIR}
+cd ${DATA_DIR}
+rm -R kaiEngine-*
