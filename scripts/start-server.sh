8f03f32326ceebd00b9afaadece7af7ea333311f
ich777 <ich777@noreply.example.org>
2020-02-04 19:12:41 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -6,16 +6,9 @@ echo "---Sleep zZz---"
 sleep infinity
 
 cd ${DATA_DIR}
-wget -q -nc --show-progress --progress=bar:force:noscroll "${DL_URI}"
-tar -xvf kaiEngine*
-rm -R kaiEngine*.tar.gz
-cd kaiEngine*
-mv * ${DATA_DIR}
-cd ${DATA_DIR}
-rm -R kaiEngine-*
 
 while true; do
-	${DATA_DIR}/kaiengine
+	${DATA_DIR}/kaiengine_x86
     echo "---KaiEngine crashed respawning---"
 	sleep 5
 done
