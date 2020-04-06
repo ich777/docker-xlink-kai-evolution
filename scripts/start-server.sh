f98d79bcd803fcf9a0a2e8be7780550c19222cf7
ich777 <ich777@noreply.example.org>
2020-04-06 14:59:10 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -56,6 +56,9 @@ else
 	fi
 fi
 
+echo "---Sleep zZz, container under construction---"
+sleep infinity
+
 echo "---Checking if 'kaiengine.conf' is present---"
 if [ ! -f ${DATA_DIR}/kaiengine.conf ]; then
 	echo "---Configuration file not found, creating---"
